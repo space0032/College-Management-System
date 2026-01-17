c7bcaf05c572e6084356e5352ab711ff78834dd9
Space <antariksh.mankar43@gmail.com>
2026-01-17 15:48:26 +0530

    feat: Grant ROOM_CHECK permission to ADMIN and FACULTY, and MANAGE_ROOMS permission to FACULTY.

--- src/main/resources/db/migration/V47__Grant_Room_Check_Permission.sql
+++ src/main/resources/db/migration/V47__Grant_Room_Check_Permission.sql
@@ -2,6 +2,15 @@
 INSERT INTO role_permissions (role_id, permission_id)
 SELECT r.id, p.id 
 FROM roles r, permissions p 
-WHERE r.code IN ('ADMIN', 'FACULTY', 'WARDEN') 
+WHERE r.code IN ('ADMIN', 'FACULTY') 
 AND p.code = 'ROOM_CHECK'
+ON CONFLICT DO NOTHING;
+
+-- Grant MANAGE_ROOMS permission to FACULTY (Admin already has it from V46)
+-- Wardens do not manage academic rooms.
+INSERT INTO role_permissions (role_id, permission_id)
+SELECT r.id, p.id 
+FROM roles r, permissions p 
+WHERE r.code = 'FACULTY' 
+AND p.code = 'MANAGE_ROOMS'
 ON CONFLICT DO NOTHING;
